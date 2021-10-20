0a70c0b78bf930b61686b2d03d8b8bfcc42b1d29
Enretro <envpogled@gmail.com>
2021-10-20 11:52:15 +0200

    photos add

--- src/data.js
+++ src/data.js
@@ -1,32 +1,33 @@
+
 export const products = [
   {
     id: 1,
-    img: "https://images.pexels.com/photos/36478/amazing-beautiful-beauty-blue.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    img: "https://images.pexels.com/photos/1040157/pexels-photo-1040157.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
     link: "https://server-more.netlify.app/"
   },
   {
     id: 2,
-    img: "https://images.pexels.com/photos/68562/pexels-photo-68562.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    img: "https://images.pexels.com/photos/3183197/pexels-photo-3183197.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
     link: "https://koktajli.netlify.app/"
   },
   {
     id: 3,
-    img: "https://images.pexels.com/photos/356043/pexels-photo-356043.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    img: "https://images.pexels.com/photos/326576/pexels-photo-326576.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
     link: "https://enspovednica.netlify.app/"
   },
   {
     id: 4,
-    img: "",
+    img: "https://images.pexels.com/photos/6684775/pexels-photo-6684775.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
     link: "https://enretro.github.io/teaser/"
   },
   {
     id: 5,
-    img: "",
+    img: "https://images.pexels.com/photos/262333/pexels-photo-262333.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
     link: "https://github.com/Enretro"
   },
   {
     id: 6,
-    img: "",
+    img: "https://images.pexels.com/photos/262508/pexels-photo-262508.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
     link: "https://app.netlify.com/teams/enretro/overview"
   }
 ]
